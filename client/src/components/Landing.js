8f8957e362d9c7705e13539995adfe17bf21cefa
Dima <benensondimitrii@gmail.com>
2022-04-18 11:57:28 +0300

    added btn image and styles on landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,5 @@
 import logo from "../assets/images/logo.svg";
-
+import main from "../assets/images/main.svg";
 
 const Landing = () => {
   return (
@@ -13,7 +13,9 @@ const Landing = () => {
                 tracking
             </span> app</h1>
             <p>I'm baby semiotics flannel austin whatever pabst. Actually literally hexagon polaroid, stumptown XOXO shoreditch wolf mumblecore aesthetic man braid pour-over. La croix food truck actually unicorn pabst taxidermy. Tote bag drinking vinegar before they sold out, copper mug schlitz sriracha next level leggings tilde kale chips. Portland ugh pitchfork hashtag, narwhal tilde knausgaard quinoa live-edge art party pinterest sriracha disrupt snackwave kogi</p>
+            <button className="btn btn-hero" >Login/Register</button>
         </div>
+        <img src={main} alt="job hunt" className="img main-img"/>
       </div>
     </main>
   );
