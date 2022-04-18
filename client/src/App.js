8f6def6e4feab0b8d41835b70c0de6d3942349e8
Dima <benensondimitrii@gmail.com>
2022-04-18 11:51:55 +0300

    adding content on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Landing from "./components/Landing";
 function App() {
   return (
     <div className="App">
+    <h1>Jobify</h1>
       <Landing/>
     </div>
   );
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,22 @@
+import logo from "../assets/images/logo.svg";
 
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo"></img>
+      </nav>
+      <div className="container-page">
+        <div className="info">
+            <h1>job <span>
+                tracking
+            </span> app</h1>
+            <p>I'm baby semiotics flannel austin whatever pabst. Actually literally hexagon polaroid, stumptown XOXO shoreditch wolf mumblecore aesthetic man braid pour-over. La croix food truck actually unicorn pabst taxidermy. Tote bag drinking vinegar before they sold out, copper mug schlitz sriracha next level leggings tilde kale chips. Portland ugh pitchfork hashtag, narwhal tilde knausgaard quinoa live-edge art party pinterest sriracha disrupt snackwave kogi</p>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Landing
+export default Landing;
