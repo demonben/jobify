77b1312169cc210e4295a79e1a13b446ca8ae615
Dima <benensondimitrii@gmail.com>
2022-04-23 10:40:06 +0300

    added global context in project

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,28 +1,36 @@
-import React, { useState, useReducer, useContext } from 'react'
+import React, { useReducer, useContext } from "react";
+import { DISPLAY_ALERT } from "./actions";
+import reducer from "./reducers";
 
 export const initialState = {
   isLoading: false,
   showAlert: false,
-  alertText: '',
-  alertType: '',
-}
-const AppContext = React.createContext()
+  alertText: "",
+  alertType: "",
+};
+const AppContext = React.createContext();
+
 const AppProvider = ({ children }) => {
-  const [state, setState] = useState(initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+  };
 
   return (
     <AppContext.Provider
       value={{
         ...state,
+        displayAlert,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useAppContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppProvider }
+export { AppProvider };
