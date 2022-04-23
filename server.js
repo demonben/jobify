98fb5ebf0471dd6fb867c8ff27053e86f08e078b
Dima <benensondimitrii@gmail.com>
2022-04-23 15:40:47 +0300

    added error handler middleware

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from "express";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 
 // middleware
 
 app.get("/",(req, res)=>{
+    throw new Error('error')
     res.send("Welcome on server")
 })
 
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5001;
 
