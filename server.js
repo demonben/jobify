d1371d99ef422fdf507b17dab11280b3f561a44c
Dima <benensondimitrii@gmail.com>
2022-05-21 11:50:31 +0300

    update server for connecting mongoDB

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
 import express from "express";
 const app = express();
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 import connectDB from "./db/connect.js";
-
 
 // middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 
-app.get("/",(req, res)=>{
-    res.send("Welcome on server")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome on server");
+});
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5001;
 
-
-const start =()=>{
+const start = async () => {
   try {
-    connectDB(process.env.MONGO_URL)
+   await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
       console.log(`server is listening ${port}...`);
     });
-    
   } catch (error) {
     console.log(error);
   }
-} 
+};
 
-start()
+start();
