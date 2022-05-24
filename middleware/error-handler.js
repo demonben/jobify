95d87584d7e4de57d12606073f12df8304699489
Dima <benensondimitrii@gmail.com>
2022-05-24 08:58:08 +0300

    updated error handler with http status code pack.

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,12 @@
-const errorHandlerMiddleware = (err, req, res, next)=>{
-    console.log(err);
-    res.status(500).json({msg: "there was an error"})
-}
-export default errorHandlerMiddleware
+import { StatusCodes } from "http-status-codes";
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  console.log(err);
+  const defaultError = {
+    StatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Something went wrong, try again later",
+  };
+
+  res.status(defaultError.StatusCode).json({ msg: err });
+};
+export default errorHandlerMiddleware;
