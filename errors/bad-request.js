c6c8002ab5d5e1f09b75e0055403638d5d15d389
Dima <benensondimitrii@gmail.com>
2022-05-25 08:58:09 +0300

    added bad-request class

---
+++ errors/bad-request.js
@@ -0,0 +1,11 @@
+import CustomAPIError from "./custom-api.js";
+import { StatusCodes } from "http-status-codes";
+
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+      super(message);
+      this.statusCodes = StatusCodes.BAD_REQUEST;
+    }
+  }
+
+  export default BadRequestError
