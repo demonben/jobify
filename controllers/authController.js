d167ed027080c588351b39b2b6b93dd90af4f90a
Dima <benensondimitrii@gmail.com>
2022-05-25 09:04:22 +0300

    updated authController validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,17 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
+
 const register = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all values");
+  }
+  const userAlreadyExists = await User.find({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("user with this email already exist");
+  }
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
