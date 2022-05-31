6bf9780780f31fc7a48ca57fd2289687a3ccc4a5
Dima <benensondimitrii@gmail.com>
2022-05-31 18:49:06 +0300

    added in the appContext and reducers registration feature

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,12 @@
 import React, { useReducer, useContext } from "react";
-import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions";
+import axios from "axios";
+import {
+  CLEAR_ALERT,
+  DISPLAY_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+} from "./actions";
 import reducer from "./reducers";
 
 export const initialState = {
@@ -7,6 +14,10 @@ export const initialState = {
   showAlert: false,
   alertText: "",
   alertType: "",
+  user: null,
+  token: null,
+  userLocation: "",
+  jobLocation: "",
 };
 const AppContext = React.createContext();
 
@@ -22,12 +33,31 @@ const AppProvider = ({ children }) => {
       dispatch({ type: CLEAR_ALERT });
     }, 2000);
   };
-
+  const registerUser = async (currentUser) => {
+    dispatch({ type: REGISTER_USER_BEGIN });
+    try {
+      const response = await axios.post("/api/v1/register", currentUser);
+      console.log(response);
+      const { user, token, location } = response.data;
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: { user, token, location },
+      });
+      //local storage later
+    } catch (error) {
+      console.log(error.response);
+      dispatch({
+        type: REGISTER_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+  };
   return (
     <AppContext.Provider
       value={{
         ...state,
         displayAlert,
+        registerUser,
       }}
     >
       {children}
