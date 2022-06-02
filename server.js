1cadd5942688750850bf4d8313dbed7d1175a036
Dima <benensondimitrii@gmail.com>
2022-06-02 10:24:30 +0300

    added morgan middleware on the server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import "express-async-errors";
+import morgan from "morgan";
+
 // db and authenticateUser
 import connectDB from "./db/connect.js";
 // routers
@@ -11,6 +13,10 @@ import jobsRoutes from "./routes/jobsRoutes.js";
 // middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
