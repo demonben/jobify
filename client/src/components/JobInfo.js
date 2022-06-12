c147b1344c1e2c56c976f089300af9941f15dc8b
Dima <benensondimitrii@gmail.com>
2022-06-12 11:26:26 +0300

    creat jog info comp.

---
+++ client/src/components/JobInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Wrapper from '../assets/wrappers/JobInfo'
+const JobInfo = () => {
+  return (
+    <Wrapper>
+    <div>JobInfo</div>
+    </Wrapper>
+  )
+}
+
+export default JobInfo
