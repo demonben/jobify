895a9803284aee53f9383a6a2f5f62ec282de600
Dima <benensondimitrii@gmail.com>
2022-06-12 11:24:16 +0300

    created search container component

---
+++ client/src/components/SearchContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FormRow, FormRowSelect } from ".";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../assets/wrappers/SearchContainer";
+
+const SearchContainer = () => {
+  return (
+    <Wrapper>
+      <div>SearchContainer</div>
+    </Wrapper>
+  );
+};
+
+export default SearchContainer;
