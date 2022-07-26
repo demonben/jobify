b540b8b00bbd209da4e85faded0deb443ae88d1b
Dima <benensondimitrii@gmail.com>
2022-07-26 20:09:22 +0300

    updated controller update job

--- controllers/jobsControllers.js
+++ controllers/jobsControllers.js
@@ -1,6 +1,10 @@
 import Job from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import {
+  BadRequestError,
+  UnAuthenticatedError,
+  NotFoundError,
+} from "../errors/index.js";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -21,8 +25,29 @@ const getAllJobs = async (req, res) => {
     .status(StatusCodes.OK)
     .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
+
 const updateJob = async (req, res) => {
-  res.send("updateJob");
+  const { id: jobId } = req.params;
+  const { company, position } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`);
+  }
+  // check permissions
+
+  checkPermissions(req.user, job.createdBy);
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
 const showStats = async (req, res) => {
   res.send("showStats");
