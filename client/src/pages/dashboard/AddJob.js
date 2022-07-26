b6d3ed6aa2baa72912cf0fa3f1c0c9bd4accc0e6
Dima <benensondimitrii@gmail.com>
2022-07-26 20:08:51 +0300

    minor fix

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -19,6 +19,7 @@ const AddJob = () => {
     handleChange,
     clearValues,
     creteJob,
+    editJob,
   } = useAppContext();
 
   const handleSubmit = (e) => {
@@ -29,7 +30,7 @@ const AddJob = () => {
       return;
     }
     if (isEditing) {
-      // eventually editJob()
+      editJob();
       return;
     }
     creteJob();
@@ -75,7 +76,7 @@ const AddJob = () => {
           <FormRowSelect
             name="jobType"
             labelText="Job type"
-            value={status}
+            value={jobType}
             handleChange={handleJobInput}
             list={jobTypeOption}
           />
