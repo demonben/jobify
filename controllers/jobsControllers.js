bfb1b12964dd540467bbe9c0f15e9aed360ecd1b
Dima <benensondimitrii@gmail.com>
2022-07-27 16:21:23 +0300

    updated jobsControllers with delete job functional

--- controllers/jobsControllers.js
+++ controllers/jobsControllers.js
@@ -18,7 +18,19 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 };
 const deleteJob = async (req, res) => {
-  res.send("deleteJob");
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.remove();
+
+  res.status(StatusCodes.OK).json({ msg: "Success! Job removed" });
 };
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId });
@@ -50,6 +62,7 @@ const updateJob = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ updatedJob });
 };
+
 const showStats = async (req, res) => {
   res.send("showStats");
 };
